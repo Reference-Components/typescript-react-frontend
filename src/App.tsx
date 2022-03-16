6c9647969d44645df44e7f6b034f731074484e43
Tomi Panula <tomi.panula@hiq.fi>
2022-03-16 10:37:02 +0200

    remove redux

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Landing from "./components/Landing";
-import { Counter } from "./features/counter/Counter";
 import TeamMemberList from "./features/teams/components/TeamMemberList";
 import UserProfile from "./features/users/components/UserProfile";
 
@@ -16,7 +15,6 @@ function App() {
           element={<UserProfile user={{ userId: "111", username: "Dummy" }} />}
         />
         <Route path="/teams" element={<TeamMemberList teamId="1234" />} />
-        <Route path="/counter" element={<Counter />} />
       </Routes>
     </BrowserRouter>
   );
